8898bae9aff2f7ed0fe17bffb92de79c082a22f6
Sergiommhh <81321755+Sergiommhh@users.noreply.github.com>
2021-03-27 15:18:34 +0100

    Update cabecera.php

--- Catálogo_p2/cabecera.php
+++ Catálogo_p2/cabecera.php
@@ -1,7 +1,7 @@
 	<header>
 
 
-		<h1> <img src="logo.png" align ="center" width="100" height="100" /> ATHENEA<h4><em>REDEFINE TUS LIMITES</em></h4>  </h1>
+		<h1> <img src="imagenes/logo.png" align ="center" width="100" height="100" /> ATHENEA<h4><em>REDEFINE TUS LIMITES</em></h4>  </h1>
 		
 	
 
