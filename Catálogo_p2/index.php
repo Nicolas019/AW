4a48f6ca93ad1b1b7a5af757f6187ca78138d464
Nicolas Benito Barrionuevo <81321728+Nicolas019@users.noreply.github.com>
2021-03-27 14:45:12 +0100

    Update index.php

--- Catálogo_p2/index.php
+++ Catálogo_p2/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="estilo.css" />
+<link rel="stylesheet" type="text/css" href="estilos.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>Catálogo</title>
 </head>
