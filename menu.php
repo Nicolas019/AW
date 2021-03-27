bd10e048d7b93fa5e3c92b408a0b6380ba165c0f
Nicolas Benito Barrionuevo <81321728+Nicolas019@users.noreply.github.com>
2021-03-27 14:23:19 +0100

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+
+ ?>
+	<nav id="menuPpal">
+		<h3>Menu</h3>
+		<p> <a href="foro.php">Inicio </a> <a href="venta.php">Ver contenido </a> <a href="retos.php">Administrar</a>  </p>
+	</nav>
