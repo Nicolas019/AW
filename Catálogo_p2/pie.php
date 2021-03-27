7bfabfb77b0504774e86bbf543232c4d1c93b288
Sergiommhh <81321755+Sergiommhh@users.noreply.github.com>
2021-03-27 15:16:11 +0100

    Update pie.php

--- Catálogo_p2/pie.php
+++ Catálogo_p2/pie.php
@@ -1,3 +1,5 @@
 <footer>
 		Copyright © 2021 Athenea. Todos los derechos reservados
+		<img src="icono_instagram.png" align ="right" width="30" height="30" />
+		<img src="icono_twitter.png" align ="right" width="30" height="30" />
 </footer>
