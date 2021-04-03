5ce12e0f5e7123900a05fcf204aacc1d44e01942
nuriacarrascosa <81321778+nuricarr@users.noreply.github.com>
2021-04-03 18:00:20 +0200

    Delete pie.php

--- pie.php
+++
@@ -1,5 +0,0 @@
-<footer>
-		Copyright © 2021 Athenea. Todos los derechos reservados
-		<img src="imagenes/icono_instagram.png" align ="right" width="30" height="30" />
-		<img src="imagenes/icono_twitter.png" align ="right" width="30" height="30" />
-</footer>
