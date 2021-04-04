7f06c7437ed59f828bedf0782c105d81dc154562
Nicolas Benito Barrionuevo <81321728+Nicolas019@users.noreply.github.com>
2021-04-04 19:46:35 +0200

    Add files via upload

--- navIzquierdo.php
+++ navIzquierdo.php
@@ -22,19 +22,19 @@
 					<h4>
 						Genero: 
 					</h4>					
-						<p> <input type="checkbox" name="Ciencia_Ficcion" value="Ciencia_Ficcion"> Ciencia Ficcion </p>
-						<p> <input type="checkbox" name="Fantasia" value="Fantasia"> Fantasia </p>
-						<p> <input type="checkbox" name="Novela" value="Novela"> Novela </p>
-						<p> <input type="checkbox" name="Novela_Historica" value="Novela_Historica"> Novela_Historica </p>
-						<p> <input type="checkbox" name="Novela_Policiaca" value="Novela_Policiaca"> Novela Policiaca </p>
-						<p> <input type="checkbox" name="Romance" value="Romance"> Romance </p>
-						<p> <input type="checkbox" name="Terror" value="Terror"> Terror	</p>	
+						<p> <input type="checkbox" name="Ciencia_Ficcion" value=1> Ciencia Ficcion </p>
+						<p> <input type="checkbox" name="Fantasia" value=2> Fantasia </p>
+						<p> <input type="checkbox" name="Novela" value=6> Novela </p>
+						<p> <input type="checkbox" name="Novela_Historica" value=7> Novela_Historica </p>
+						<p> <input type="checkbox" name="Novela_Policiaca" value=4> Novela Policiaca </p>
+						<p> <input type="checkbox" name="Romance" value=3> Romance </p>
+						<p> <input type="checkbox" name="Terror" value=5> Terror	</p>	
 					
 					<h4> Precio mínimo: </h4>
-					<p> <input type="text" name="precioMin"> </p>
+					<p> <input type="text" name="precioMin" value=0> </p>
 
 					<h4>Precio máximo: </h4>
-					<p> <input type="text" name="precioMax"> </p>
+					<p> <input type="text" name="precioMax" value=100> </p>
 
 					<button type="submit" name ="submit" value ="Enviar_form">
 						<img src="imagenes/filtro.png" width="30" height="30" background_color="white"> 
