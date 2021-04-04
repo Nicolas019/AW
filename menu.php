3d36b9ec0fbd535a2eef6d9df41b2b739df8be64
Nicolas Benito Barrionuevo <81321728+Nicolas019@users.noreply.github.com>
2021-04-04 21:56:30 +0200

    Add files via upload

--- menu.php
+++ menu.php
@@ -1,23 +1,7 @@
 	<nav id="menuPortada">
 		
 		<ul id="NavHorizontal">
-			<li>
-				<form action ="index.php" method="GET">
-					<select name="opcion"> 
-						<option value="Sin Ordenar"> Sin ordenar </option>  
-						<option value="Ciencia Ficcion"> Ciencia Ficcion </option>
-						<option value="Fantasia"> Fantasia </option>
-						<option value="Novela"> Novela </option>
-						<option value="Novela Historica"> Novela Historica </option>
-						<option value="Novela Policiaca"> Novela Policiaca </option>
-						<option value="Romance"> Romance </option>
-						<option value="Terror"> Terror </option>
-					</select>
-					<button type="submit" name ="submit" value ="Enviar_form">
-						Enviar
-					</button>
-				</form>
-			</li>
+
 			<li> <a href="biblioteca.php">Biblioteca </a> </li> 
 			<li> <a href="foro.php">Foro </a> </li> 
 			<li> <a href="venta.php">Venta</a> </li>
@@ -26,14 +10,27 @@
 				session_start();
 
 				if(!isset($_SESSION['login']) || $_SESSION['login'] == false){
-					echo "<li> <a href='login.php'><img src='imagenes/icono_perfil.png' align ='right' width='30' height='30' /></a> </li>";
+					echo "<li id=\"alinearDerecha\"> <a href='login.php'><img src='imagenes/icono_perfil.png' width='30' height='30' /></a> </li>";
 					//echo "<li> <a href='login.php'>Iniciar Sesion</a> </li>";
 				}
 				else{
-					echo "<li><a href='perfil.php'><img src='imagenes/icono_perfil.png' align ='right' width='30' height='30' /></a> </li>";
+					echo "<li id=\"alinearDerecha\"><a href='perfil.php'><img src='imagenes/icono_perfil.png' width='30' height='30' /></a> </li>";
 				}
 			?>
-			<li> <a href='carrito.php'><img src='imagenes/icono_carrito.png' align ='right' width='30' height='30' /></a> </li>
-			<li> Buscar <img src='imagenes/icono_lupa.png' align ='right' width='30' height='30' /></li>
+			<li id="alinearDerecha"> <a href='carrito.php'><img src='imagenes/icono_carrito.png'  width='30' height='30' /></a> </li>
+
+			<li id="alinearDerecha"> 
+
+				<form action ="biblioteca.php" method="GET" align="center"> 
+					
+					<input type="text" name="buscar" value="Busqueda">
+					<button type="submit" name ="submit" value ="Enviar_form">
+						<img src='imagenes/icono_lupa.png' width='30' height='30' />
+					</button>
+
+				</form>
+
+				
+			</li>
 		</ul>   
 	</nav>
--- menuPrincipal.php
+++ menuPrincipal.php
@@ -2,26 +2,6 @@
 		
 		<ul id="NavHorizontal">
 			<li><a href="index.php"><img src="imagenes/logo.png" align ="center" width="50" height="50" /> </a></li>
-			<li>
-				<form action ="biblioteca.php" method="GET">
-					
-					<select name="orden"> 
-						<option value="Sin Ordenar"> Sin ordenar </option>  
-						<option value="Valoracion"> Valoración </option>
-						<option value="Alfabetico"> Alfabético </option>
-						<option value="Fecha"> Fecha </option>
-						<option value="NumPaginas"> Numero de páginas </option>
-						<option value = "Ventas">Numero de ventas </option>
-					</select>
-					<select name="sentido"> 
-						<option value="ASC"> Ascendente </option>  
-						<option value="DESC"> Descendente</option>
-					</select>
-					<button type="submit" name ="submit" value ="Enviar_form">
-						Enviar
-					</button>
-				</form>
-			</li>
 			<li> <a href="biblioteca.php">Biblioteca </a> </li> 
 			<li> <a href="foro.php">Foro </a> </li> 
 			<li> <a href="venta.php">Venta</a> </li>
@@ -30,14 +10,28 @@
 				session_start();
 
 				if(!isset($_SESSION['login']) || $_SESSION['login'] == false){
-					echo "<li> <a href='login.php'><img src='imagenes/icono_perfil.png' align ='right' width='30' height='30' /></a> </li>";
+					echo "<li id=\"alinearDerecha\"> <a href='login.php'><img src='imagenes/icono_perfil.png'  width='30' height='30' /></a> </li>";
 					//echo "<li> <a href='login.php'>Iniciar Sesion</a> </li>";
 				}
 				else{
-					echo "<li><a href='perfil.php'><img src='imagenes/icono_perfil.png' align ='right' width='30' height='30' /></a> </li>";
+					echo "<li id=\"alinearDerecha\"><a href='perfil.php'><img src='imagenes/icono_perfil.png'  width='30' height='30' /></a> </li>";
 				}
 			?>
-			<li> <a href='carrito.php'><img src='imagenes/icono_carrito.png' align ='right' width='30' height='30' /></a> </li>
-			<li> Buscar <img src='imagenes/icono_lupa.png' align ='right' width='30' height='30' /></li>
+			<li id="alinearDerecha"> <a href='carrito.php'><img src='imagenes/icono_carrito.png'  width='30' height='30' /></a> </li>
+
+			<li id="alinearDerecha"> 
+
+				<form action ="biblioteca.php" method="GET"> 
+					
+					<input type="text" name="buscar" value="Busqueda">
+					<button type="submit" name ="submit" value ="Enviar_form">
+						<img src='imagenes/icono_lupa.png' width='30' height='30' />
+					</button>
+
+				</form>
+
+				
+			</li>
+
 		</ul>   
 	</header>
