f20e4d295ac72d8660a638edf7575e34f7e3b6cd
nuriacarrascosa <81321778+nuricarr@users.noreply.github.com>
2021-04-11 14:14:58 +0200

    Add files via upload

--- verLibro.php
+++ verLibro.php
@@ -1,3 +1,35 @@
+<?php
+    require_once '../comun/BD.php';
+    require_once '../comun/libroEnVenta.php';
+    
+    $init_sesion = (!isset($_SESSION["login"]) || $_SESSION["login"] === false) ? false : true;
+    
+    //Libro en venta
+    $id_Libro = isset($_GET["id_Libro"]) ? htmlspecialchars(trim(strip_tags($_GET["id_Libro"]))) : 0;
+    $book = new libroEnVenta($id_Libro);
+    $titulo = $book->$titulo;
+    $autor = $book->$autor;
+    $genero = $book->$genero;
+    $editorial = $book->$editorial;
+    $ruta_imagen = $book->$ruta_imagen;
+    $valoracion = $book->$valoracion;
+    $num_stars = $book->$numStars;
+    $num_pags = $book->$numPag;
+    $sinopsis = $book->$sinopsis;
+    $fecha_lanzamiento = $book->$fecha;
+
+    //Almacen
+    $hay_stock = $book->hay_stock_libroEnVenta();
+    $precio_nuevo = ($book->precio_libroEnVenta('nuevo') != 0) ? $book->precio_libroEnVenta('nuevo') : 0;
+    $precio_como_nuevo = ($book->precio_libroEnVenta('como nuevo') != 0) ? $book->precio_libroEnVenta('como nuevo') : 0;
+    $precio_buen_estado = ($book->precio_libroEnVenta('buen estado') != 0) ? $book->precio_libroEnVenta('buen estado') : 0;
+    $precio_aceptable = ($book->precio_libroEnVenta('aceptable') != 0) ? $book->precio_libroEnVenta('aceptable') : 0;
+
+    //Desconectar base de datos
+    $book->desconectarBD();
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,63 +41,54 @@
 
 <div id="contenedor">
 <?php
-		require '../comun/menuPrincipal.php';
-		require '../comun/BD.php';
-        require '../comun/libro.php';
-        require '../comun/almacen.php';
-        require '../comun/comentarios.php';
+	require_once '../comun/menuPrincipal.php';
 ?>
 <main id="contenido">
 
     <?php
-
-    //Mostrar información del libro
-    $id_Libro = $_GET["id_Libro"];
-    $libro = new libro($id_Libro);
-    $libro->ver_libro();
-
-    //Comprobar si hay stock y mostrar los diferentes estados
-    $almacen_libros = new almacen();
-    if(!$almacen_libros->hay_stock($id_Libro)){
-        //no hay stock
-        echo "<br>No hay stock disponible.</br>";
-    }
-    else{
-        //si hay stock
-        if(!isset($_SESSION["login"]) || $_SESSION["login"] === false){
-            echo "<br>Hay que iniciar sesión para comprar.</br>";
+        echo "<h2>".$titulo."</h2>";
+        echo "<h3>".$autor."</h3>";
+        echo "<br>Valoración: ".$valoracion."</br>";
+        $i = 0;
+        while($numStars > $i){
+            echo "<img id=\"valor\" src=\"../comun/imagenes","/","star.png","\">";
+            $i = $i + 1;
         }
-        else{
-            $estado_nuevo = ($almacen_libros->precio_nuevo() != 0);  
-            $estado_comonuevo = ($almacen_libros->precio_como_nuevo() != 0);  
-            $estado_buenestado = ($almacen_libros->precio_buen_estado() != 0); 
-            $estado_aceptable = ($almacen_libros->precio_aceptable() != 0); 
 
+        echo "<br>Género: ".$genero."</br>";
+        echo "<br>Editorial: ".$editorial."</br>";
+
+        if($num_pags != NULL){
+            
+        }
+        echo "<br>Número de páginas: ".$num_pags."</br>";
+
+        if($fecha_lanzamiento != NULL){
+            echo "<br>Fecha de lanzamiento: ".$fecha_lanzamiento."</br>";
+        }
+        echo "<br><img id=\"ver_libro\" src=\"../comun/imagenes","/",$ruta_img,"\"></br>";
+        echo "<br>Sinopsis: ".$sinopsis."</br>";
     ?>
 
-        <br>Precios:</br>
-        <form action ="../compra/verCarrito.php" method="POST">
+    <br>Precios:</br>
+    <form action ="../compra/verCarrito.php" method="POST">
 					
 		<select name="precio_libro"> 
 		<?php
-			if($estado_nuevo === true){
-                $precio_nuevo = $almacen_libros->precio_nuevo();
-				echo "<option value=$almacen_libros->precio_nuevo()> Nuevo ($precio_nuevo) </option>";				
+			if($precio_nuevo != 0){
+				echo "<option value=$precio_nuevo> Nuevo ($precio_nuevo) </option>";
 			}
 						
-			if($estado_comonuevo === true){
-                $precio_como_nuevo = $almacen_libros->precio_como_nuevo();
-				echo "<option value=$almacen_libros->precio_como_nuevo()> Como nuevo ($precio_como_nuevo) </option>";
+			if($precio_como_nuevo != 0){
+				echo "<option value=$precio_como_nuevo Como nuevo ($precio_como_nuevo) </option>";
 			}
 			
-			if($estado_buenestado === true){
-                $precio_buen_estado = $almacen_libros->precio_buen_estado();
-				echo "<option value=$almacen_libros->precio_buen_estado()> Buen estado ($precio_buen_estado) </option>";
+			if($precio_buen_estado != 0){
+				echo "<option value=$precio_buen_estado> Buen estado ($precio_buen_estado) </option>";
 			}
 		
-			if($estado_aceptable === true){
-                $precio_aceptable = $almacen_libros->precio_aceptable();
-				echo "<option value=$almacen_libros->precio_aceptable()> Aceptable ($precio_aceptable) </option>";
+			if($precio_aceptable != 0){
+				echo "<option value=$precio_aceptable> Aceptable ($precio_aceptable) </option>";
 			}
 		?>
 		</select>			
@@ -75,12 +98,11 @@
 
 		<button type="submit" name ="submit" value ="add_carrito">Añadir al carrito</button>
             
-	   </form>
+	</form>
 
 
     <?php
-        }
-    }
+
     $comentario = new comentarios();
 
     $nuevo_comentario = isset($_GET["comentario_nuevo"]) ? $_GET["comentario_nuevo"] : null;
@@ -115,7 +137,7 @@
 </main>
 	
 <?php 
-require '../comun/pie.php';
+require_once '../comun/pie.php';
 ?>
 
 </div> <!-- Fin del contenedor -->
